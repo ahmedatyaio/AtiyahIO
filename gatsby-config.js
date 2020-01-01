a47579c339d20ae6b7f0d0a9acda3c32ef4a7020
Ahmed Atiyah <ahmedmaheratiyah@gmail.com>
2020-01-02 01:34:46 +0200

    setting up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ahmed M. Atiyah, Fullstack Web Developer`,
+    description: `Egypt based, Fullstack Web Developer ready for Freelance and Part Time Jobs.`,
+    author: `@atiyahio`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,25 +10,31 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `atiyah-io`,
+        short_name: `Atiyah`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png`
+      }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`montserrat\:700`, `rubik`],
+        display: 'swap'
+      }
+    },
+    `gatsby-plugin-styled-components`
+  ]
+};
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  fonts: {
+    pri: 'Montserrat, sans-serif',
+    sec: 'Rubik, sans-serif'
+  },
+  responsive: {
+    mw: '1200px'
+  },
+  colors: {
+    green: '#07D495',
+    green2: '#067553',
+    grey: '#C2C2C2',
+    grey2: '#707070'
+  }
+};
+
+export default theme;
